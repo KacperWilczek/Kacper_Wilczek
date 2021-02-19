7b825aab5c1c9d4e9638cc251cad5e6d064a67a0
Programista15 <Programista15@pzs1ws.local>
2021-02-19 13:39:45 +0100

    koniec

--- skrypt.js
+++ skrypt.js
@@ -22,6 +22,26 @@ function stoper()
 
 
 
+function obliczenia()
+{
+    var wartosc_a =parseInt(document.getElementById("wartosc_a").value);
+    var wartosc_b = parseInt(document.getElementById("wartosc_b").value);
+    var wartosc_c = parseInt(document.getElementById("wartosc_c").value); 
+    var wynik1 = 0;
+    var wynik2 = 0;
+
+    wynik1 = Math.pow(wartosc_a,wartosc_b) - wartosc_c;
 
 
+    wynik2 = if(wartosc_b != 0)
+    {
+        (wartosc_a/wartosc_b)–(wartosc_a*wartosc_c) 
+    };
+
+    wynik3 = if(wartosc_a !=0 || wartosc_b !=0 ){
+       (wartosc_c/wartosc_b) + (wartosc_b/wartosc_a)
+    } ;
+
+    document.getElementById("pusty").innerText = wynik1 + " " + wynik2 + " " + wynik3;
+}
 
